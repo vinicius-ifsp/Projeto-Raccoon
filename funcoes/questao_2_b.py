f4493b022ce0bc7d263e46433d5114491beacc42
Vinicius Luiz da Silva <36741834+vinicius-ifsp@users.noreply.github.com>
2019-03-29 17:01:00 -0300

    Update questao_2_b.py

--- funcoes/questao_2_b.py
+++ funcoes/questao_2_b.py
@@ -1,31 +1,3 @@
-import json
-import re
-
-
-# Questao 1 - A
-def fixName(data):
-    # dicionario com '\'+ caracter corrompido como chave e a letra correspondente correta como valor
-    dic_regex = {"\\æ": "a", "\\¢": "c", "\\ø": "o", "\\ß": "b"}
-    # monta expressao regular com as chaves do dicionario
-    pattern = re.compile("|".join(dic_regex.keys()))
-    # loop para percorrer os dados
-    for i in range(len(data)):
-        # Aplica a expressão em cima do nome da iteracao atual do loop
-        data[i]["name"] = pattern.sub(
-            lambda letra: dic_regex[re.escape(letra.group(0))], data[i]["name"])
-
-
-# Questao 1 - B
-def fixPrice(data):
-    # loop para percorrer os dados
-    for i in range(len(data)):
-        # verifica se o numero nao esta como float
-        if not isinstance(data[i]["price"], float):
-            # caso nao esteja realiza a conversao
-            data[i]["price"] = float(data[i]["price"])
-
-
-# Questao 1 - C
 # Questao 2 - B
 def calculatorTotalStockValueByCategory(data):
     dic_sotck = {}
